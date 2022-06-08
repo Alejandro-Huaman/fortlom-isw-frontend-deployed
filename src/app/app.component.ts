b3339d3d03aaf5564edea2cc0f168ec5df3456df
Alejandro-Huaman <90795926+Alejandro-Huaman@users.noreply.github.com>
2022-06-08 14:36:35 -0500

    final commit frontend deployed with google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import { filter } from 'rxjs/operators';
+import {OnInit} from '@angular/core';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'fortlom-isw-f';
+
+  constructor(private router: Router){
+  
+  }
+
+  ngOnInit(){
+    this.setUpAnalitics();    
+  }
+
+  setUpAnalitics(){
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe((event:any) => {
+        gtag('config', 'G-PYFYP1WSD7', {
+          'page_path': event.urlAfterRedirects
+        });
+      });
+  }
 }
